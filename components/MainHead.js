c3b4366c7b52e0f658f4f059a94f9efaeb749734
Juan Martín García <hello@juangarcia.com.ar>
2023-03-02 17:49:23 -0300

    Add pixel code

--- components/MainHead.js
+++ components/MainHead.js
@@ -35,6 +35,8 @@ export default function MainHead({ pageTitle }) {
       {/* Scripts */}
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       <script type="text/javascript" src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}`} />
+
+      {/* GTM */}
       <script
         defer async
         dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -43,6 +45,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TJS2ZPZ');`}}
       />
+
+      {/* Facebook Pixel */}
+      <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '173450402104558'); 
+        fbq('track', 'PageView');`}} />
+      <noscript>
+        <img height="1" width="1" src="https://www.facebook.com/tr?id=173450402104558&ev=PageView&noscript=1"/>
+      </noscript>
     </Head>
   );
 }
