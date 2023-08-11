3c795e261640c379d01de6c6aa654da5bbb39cca
Juan Martín García <hello@juangarcia.com.ar>
2023-08-11 19:53:50 -0300

    Update arrows for hero carousel

--- components/Home/HeroSlider.js
+++ components/Home/HeroSlider.js
@@ -16,6 +16,33 @@ import { horizontalPadding } from "../../classes/Spacing";
 import TitlePackage from "../TitlePackage";
 
 export default function HeroSlider({ slider }) {
+  
+  const PrevArrow = (props) => {
+    const { onClick } = props;
+
+    return (
+      <button
+        className="z-10 absolute transition duration-100 hover:opacity-80 h-full px-4 w-auto hidden lg:block left-0"
+        onClick={onClick}
+      >
+        <svg fill="none" height="22" viewBox="0 0 11 22" width="11" xmlns="http://www.w3.org/2000/svg"><path d="m10.4688.734347c.1874.140625.2343.328123.2343.515623s-.0469.375-.1875.51563l-8.53122 9.2344 8.48442 9.2812c.2812.2813.2812.75-.0469 1.0313-.2813.2812-.75002.2812-1.03128-.0469l-8.999995-9.75c-.28125-.2813-.28125-.7031 0-.9844l8.999995-9.749978c.32813-.328125.79688-.328125 1.07818-.046875z" fill="#fff"/></svg>
+      </button>
+    )
+  }
+  
+  const NextArrow = (props) => {
+    const { onClick } = props;
+
+    return (
+      <button
+        className="z-10 absolute transition duration-100 hover:opacity-80 h-full px-4 w-auto hidden lg:block top-0 right-0"
+        onClick={onClick}
+      >
+        <svg fill="none" height="22" viewBox="0 0 11 22" width="11" xmlns="http://www.w3.org/2000/svg"><path d="m.484375 21.3125c-.1875-.1407-.28125-.3282-.28125-.5157s.09375-.375.234375-.5156l8.48438-9.2344-8.48438-9.2812c-.28125-.28125-.28125-.75.046875-1.031253.28125-.28125.750005-.28125 1.031245.046875l8.99998 9.749978c.2813.2813.2813.7031 0 .9844l-8.99998 9.75c-.28124.3281-.749995.3281-1.031245.0469z" fill="#fff"/></svg>
+      </button>
+    )
+  }
+
   const settings = {
     dots: true,
     arrows: true,
@@ -27,6 +54,8 @@ export default function HeroSlider({ slider }) {
     slidesToScroll: 1,
     dotsClass: styles.HeroSliderDots,
     draggable: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
 
   return (
@@ -35,7 +64,7 @@ export default function HeroSlider({ slider }) {
     >
       <Slider
         {...settings} 
-        className={`w-full ${styles.HeroSlider}`}
+        className="w-full relative"
       >
         {slider
           .map((content, index) => {
--- content/homepage.md
+++ content/homepage.md
@@ -6,4 +6,9 @@ heroSlider:
     text: Isolant te ayuda a elegir bien el aislante para tus obras. Nuestro departamento técnico está listo para responder a todas tus consultas.
     ctaText: Conocenos
     ctaLink: /nosotros
+  - image: /images/homepage/slider/slide-1.jpg
+    title: Toda una empresa <br /><strong>a tu servicio</strong>
+    text: Isolant te ayuda a elegir bien el aislante para tus obras. Nuestro departamento técnico está listo para responder a todas tus consultas.
+    ctaText: Conocenos
+    ctaLink: /nosotros
 ---
