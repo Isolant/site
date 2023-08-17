b0c89e85a27e862d26aceec24e7459f601a0034f
Juan Martín García <hello@juangarcia.com.ar>
2023-08-17 10:33:09 -0300

    Fix missing key in iterator

--- components/Products/ProductList.js
+++ components/Products/ProductList.js
@@ -9,9 +9,10 @@ export default function ProductList({
   return (
     <article className="md:col-span-2 lg:col-span-9">
       <ul className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
-        {products.map((product) =>
+        {products.map((product, index) =>
           <ProductListCard
             product={product}
+            key={index}
           />
         )}
       </ul>
